97f227f4b77d1437815eb1a8be90d8c0e31c71e5
narakulayeshwanth <152397775+narakulayeshwanth@users.noreply.github.com>
2026-06-25 23:47:47 +0530

    Time: 77 ms (87.56%), Space: 19.7 MB (9.33%) - LeetHub

---
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.py
@@ -0,0 +1,36 @@
+class Solution:
+    def countMajoritySubarrays(self, nums, target):
+        n = len(nums)
+
+        size = 2 * n + 5
+        bit = [0] * size
+
+        def update(i):
+            while i < size:
+                bit[i] += 1
+                i += i & -i
+
+        def query(i):
+            s = 0
+            while i > 0:
+                s += bit[i]
+                i -= i & -i
+            return s
+
+        offset = n + 2
+        pref = 0
+        ans = 0
+
+        update(offset)
+
+        for x in nums:
+            if x == target:
+                pref += 1
+            else:
+                pref -= 1
+
+            idx = pref + offset
+            ans += query(idx - 1)   
+            update(idx)
+
+        return ans
