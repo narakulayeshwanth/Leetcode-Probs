172becbc59b2830d90fd44c9b408bae6900ea066
narakulayeshwanth <152397775+narakulayeshwanth@users.noreply.github.com>
2026-07-10 16:24:09 +0530

    Time: 0 ms (100%), Space: 19.3 MB (61.39%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        while n > 1:
+            if (n & 1) == ((n >> 1) & 1):
+                return False
+            n >>= 1
+        return True
